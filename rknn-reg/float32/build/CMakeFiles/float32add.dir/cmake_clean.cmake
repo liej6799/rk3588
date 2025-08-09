912d76a1e77b816ca6082c0fe90e1163c59d0d8a
joeslie <liej6799@protonmail.ch>
2025-08-09 21:20:22 +0700

    gen int and float32 reg

---
+++ rknn-reg/float32/build/CMakeFiles/float32add.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/float32add.dir/float32add.cpp.o"
+  "CMakeFiles/float32add.dir/float32add.cpp.o.d"
+  "float32add"
+  "float32add.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/float32add.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ rknn-reg/int/build_and_run.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+
+# Always regenerate the model
+python3 int32add.py
+
+# Create and enter build directory
+BUILD_DIR=build
+mkdir -p "$BUILD_DIR"
+cd "$BUILD_DIR"
+
+# Configure and build
+cmake ..
+make
+
+# Run the executable with any passed arguments
+echo "Running ./int32add ..."
+LD_LIBRARY_PATH="/root/.pyenv/versions/3.11.4/lib" LD_PRELOAD="/data/Dev/rk3588/rknn-sniff/preload_python.so" ./int32add "$@" 
